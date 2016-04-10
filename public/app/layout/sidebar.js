d9b1b9d1fb8b3b68c2dcfa72eb1edad1d241e631
Clay Rardin <clayton.rardin@gmail.com>
2016-04-10 02:55:55 -0600

    stable nag version

--- public/app/layout/sidebar.js
+++ public/app/layout/sidebar.js
@@ -9,14 +9,32 @@
         var vm = this;
 
         vm.isCurrent = isCurrent;
+        vm.collapse1 = false;
+        vm.collapse2 = true;
+        vm.collapse3 = true;
+        vm.toggleCollapse = toggleCollapse;
 
         activate();
 
-        function activate() { getNavRoutes(); }
-        
+        function activate() {
+            getNavRoutes();
+        }
+
+        function toggleCollapse(currentValue, collapseValue){
+            // toggles the value of the collapse variable
+            if (collapseValue == 1){
+                return vm.collapse1 = !currentValue;
+            } else if (collapseValue == 2){
+                return vm.collapse2 = !currentValue;
+            } else if (collapseValue == 3){
+                return vm.collapse3 = !currentValue;
+            }
+        }
+
         function getNavRoutes() {
             vm.navRoutes = routes.filter(function(r) {
-                return r.config.settings && r.config.settings.nav;
+                //return r.config.settings && r.config.settings.nav;
+                return r.config.settings && r.config.settings.menu;
             }).sort(function(r1, r2) {
                 return r1.config.settings.nav - r2.config.settings.nav;
             });
